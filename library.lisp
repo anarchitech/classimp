a4eca4a8bc0137291318d256b58a5be9a8c95e30
Cthulhu <cthulhu@neuromancer>
2020-01-05 09:13:25 -0800

    Tweak path to allow classimp to play nicely w/ NixOS. For 'fraggle' there are other packages that will need to be downloaded as repos into quicklisp/local-projects and then have their respective libraries tweaked by hand.  Not sure if we'll fork those here in the collective's public repo or not ... 	modified:   library.lisp

--- library.lisp
+++ library.lisp
@@ -4,6 +4,6 @@
   (:darwin "libassimp.dylib")
   (:windows (:or "assimp.dll" "libassimp.dll"));; :calling-convention :stdcall ?
   #++(:unix "libassimp.so")
-  (:unix (:or "libassimp.so.4" "libassimp.so.4.1.0" "libassimp.so")))
+  (:unix (:or "libassimp.so.4" "libassimp.so.4.1.0" "libassimp.so" "~/.nix-profile/lib/libassimp.so.4" "~/.nix-profile/lib/libassimp.so.4.1.0" "~/.nix-profile/lib/libassimp.so")))
 
 (use-foreign-library assimp)
