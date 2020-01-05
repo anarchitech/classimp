650b1e2886ebcd1e291cbdccf29fd9844facb538
Cthulhu <cthulhu@neuromancer>
2020-01-05 09:21:41 -0800

    Add a note to library.lisp re: NixOS paths.
    	modified:   library.lisp

--- library.lisp
+++ library.lisp
@@ -4,6 +4,10 @@
   (:darwin "libassimp.dylib")
   (:windows (:or "assimp.dll" "libassimp.dll"));; :calling-convention :stdcall ?
   #++(:unix "libassimp.so")
+
+  ;; NixOS stores its per user path information in $HOME/.nix-profile,
+  ;; so we need to aim at that too if we're playing on that system.
+  
   (:unix (:or "libassimp.so.4" "libassimp.so.4.1.0" "libassimp.so" "~/.nix-profile/lib/libassimp.so.4" "~/.nix-profile/lib/libassimp.so.4.1.0" "~/.nix-profile/lib/libassimp.so")))
 
 (use-foreign-library assimp)
